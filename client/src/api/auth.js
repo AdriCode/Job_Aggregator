0a8f9d6dba1abd0e2883b2cea79f6fa2bb5ed74b
NotAdri Code <147796305+AdriCode@users.noreply.github.com>
2026-06-18 20:06:10 +0530

    use env variable for API base url

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8000/api';
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // attach token to every request automatically
 axios.interceptors.request.use(config => {
